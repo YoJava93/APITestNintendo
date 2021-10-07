7e717affe3071a164ec33e777a83fa816788a505
Alexandr Corenciuc <alexandrcorenciuc@gmail.com>
2021-10-06 23:11:57 -0700

    finished testing negative POST request

--- src/test/java/nintendo/step_definition/APIVerification.java
+++ src/test/java/nintendo/step_definition/APIVerification.java
@@ -6,6 +6,10 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import nintendo.pojo.SKUs;
 import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 
 public class APIVerification {
@@ -14,10 +18,10 @@ public class APIVerification {
 
     @Given("I logged to SKUs api using {string} and {string} to generate Token")
     public void i_logged_to_SKUS_api_using_and(String username, String password) {
-        // i do not have Authorization params, but i wrote it just to simulate real work env
+        // this is OPEN API and i do not need to authorize, but i wrote it just to simulate real work env
     }
 
-    @When("I send a post request using pojo class")
+    @When("I send a POST request using pojo class")
     public void i_send_a_post_request_usin_pojo_class() {
         SKUs skus = new SKUs();
         skus.setSku("sleeping_bag");
@@ -41,7 +45,7 @@ public class APIVerification {
 
 
 
-    @When("I sent a get request with id {string} and i check response against JSon Schema Validation to verify if response body if it matches with requirements")
+    @When("I sent a GET request with id {string} and i check response against JSon Schema Validation to verify if response body if it matches with requirements")
     public void i_sent_a_get_request_with_id(String inputId) {
 
         Integer id = Integer.valueOf(inputId);
@@ -49,11 +53,11 @@ public class APIVerification {
          response = given().accept(ContentType.JSON)
                 .and()
                 .pathParam("id", id)
-                .when()
+         .when()
                 .get("https://1ryu4whyek.execute-api.us-west-2.amazonaws.com/dev/skus/{id}")
-                .then()
+         .then()
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("features/SKUSchema.json"))
-                .extract().response();
+         .extract().response();
     }
 
     @When("I  send DELETE request  with id {string}")
@@ -64,13 +68,13 @@ public class APIVerification {
          response = given().accept(ContentType.JSON)
                 .and()
                 .pathParam("id", id)
-                .when()
+         .when()
                 .delete("https://1ryu4whyek.execute-api.us-west-2.amazonaws.com/dev/skus/{id}")
-                .then().extract().response();
+         .then().extract().response();
 
     }
 
-    @Then("I send GET request to {string}")
+    @Then("I send GET request to id {string}")
     public void i_send_GET_request_to(String inputId) {
 
         Integer id = Integer.valueOf(inputId);
@@ -78,10 +82,10 @@ public class APIVerification {
         response = given().accept(ContentType.JSON)
                 .and()
                 .pathParam("id", id)
-                .when()
+        .when()
                 .get("https://1ryu4whyek.execute-api.us-west-2.amazonaws.com/dev/skus/{id}")
                 .then()
-                .extract().response();
+        .extract().response();
     }
 
 
@@ -89,8 +93,23 @@ public class APIVerification {
     public void i_send_GET_request() {
         response = given().accept(ContentType.JSON)
                 .and()
-                .when()
+        .when()
                 .get("https://1ryu4whyek.execute-api.us-west-2.amazonaws.com/dev/skus")
+                .then()
+        .extract().response();
+    }
+
+    @When("I send POST request with invalid body")
+    public void i_send_POST_request_with_invalid_body() {
+        Map<String, String> map = new HashMap<>();
+        map.put("ku","sleeping_bag");
+        map.put("description","Outdoor");
+        map.put("price","49.99");
+
+        response = given().accept(ContentType.JSON)
+                .and().body(map)
+                .when()
+                .post("https://1ryu4whyek.execute-api.us-west-2.amazonaws.com/dev/skus")
                 .then()
                 .extract().response();
     }
--- src/test/resources/features/skus.feature
+++ src/test/resources/features/skus.feature
@@ -3,24 +3,27 @@ Feature: API verification
   Background: Assuming user is has to login each time when sending any requests
     Given I logged to SKUs api using "username" and "password" to generate Token
 
-  @wip
   Scenario: Testing GET request
     When I send GET request
     Then Status code should be 200
 
   Scenario:Create a product and verify status code
-    When I send a post request using pojo class
+    When I send a POST request using pojo class
     Then Status code should be 200
 
     Scenario: Checking JSON Schema validation against response body
-      When I sent a get request with id "20" and i check response against JSon Schema Validation to verify if response body if it matches with requirements
+      When I sent a GET request with id "20" and i check response against JSon Schema Validation to verify if response body if it matches with requirements
       And  Status code should be 200
 
     Scenario: Testing delete request
       When I  send DELETE request  with id "20"
       Then Status code should be 200
-      And I send GET request to "20"
-      And Status code should be 403
+      And I send GET request to id "20"
+      And Status code should be 404
+  @wip
+      Scenario:Negative POST request test
+        When I send POST request with invalid body
+        Then Status code should be 400
 
 
 
